7fb48882bab026a61923bdace37e4092cc2ad6d1
fanyi.lzj <fanyi.lzj@alibaba-inc.com>
2021-08-27 16:47:28 +0800

    core: support load config from api

--- packages/hooks-core/src/config/index.ts
+++ packages/hooks-core/src/config/index.ts
@@ -15,23 +15,16 @@ export function getProjectRoot(cwd?: string) {
   return sync(cwd) || process.cwd()
 }
 
+export const PRE_DEFINE_PROJECT_CONFIG = Symbol.for('PRE_DEFINE_PROJECT_CONFIG')
+
+export function setConfig(config: Partial<ProjectConfig>) {
+  globalThis[PRE_DEFINE_PROJECT_CONFIG] = config
+}
+
 export function getConfig(cwd?: string): ProjectConfig {
-  const root = getProjectRoot(cwd)
-
-  const configs = {
-    ts: path.join(root, 'midway.config.ts'),
-    js: path.join(root, 'midway.config.js'),
-  }
-
-  if (!existsSync(configs.ts) && !existsSync(configs.js)) {
-    throw new Error(
-      `[ERR_INVALID_CONFIG] midway.config.ts is not found, root: ${root}`
-    )
-  }
-
-  const userConfig = existsSync(configs.ts)
-    ? requireByJiti<UserConfig>(configs.ts)
-    : requireByJiti<UserConfig>(configs.js)
+  const userConfig = globalThis[PRE_DEFINE_PROJECT_CONFIG]
+    ? globalThis[PRE_DEFINE_PROJECT_CONFIG]
+    : getConfigFromFile(cwd)
 
   const builtinGateways = new Set<HooksGatewayAdapterStatic>()
   for (const route of userConfig.routes) {
@@ -54,6 +47,27 @@ export function getConfig(cwd?: string): ProjectConfig {
       client: '@midwayjs/hooks-core/request',
     },
   })
+}
+
+function getConfigFromFile(cwd?: string) {
+  const root = getProjectRoot(cwd)
+
+  const configs = {
+    ts: path.join(root, 'midway.config.ts'),
+    js: path.join(root, 'midway.config.js'),
+  }
+
+  if (!existsSync(configs.ts) && !existsSync(configs.js)) {
+    throw new Error(
+      `[ERR_INVALID_CONFIG] midway.config.ts is not found, root: ${root}`
+    )
+  }
+
+  const userConfig = existsSync(configs.ts)
+    ? requireByJiti<UserConfig>(configs.ts)
+    : requireByJiti<UserConfig>(configs.js)
+
+  return userConfig
 }
 
 export function defineConfig(config: UserConfig): UserConfig {
